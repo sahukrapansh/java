57b34699bb320d59b6c186a835ede7f8d3e5109d
KRAPANSH SAHU <sahukrapansh@gmail.com>
2026-04-08 17:00:13 +0000

    updates

---
+++ 01_basics/patern.java
@@ -0,0 +1,8 @@
+public class patern {
+ public static void main(String args[]){
+    System.out.println("* * * *");
+    System.out.println("* * * ");
+    System.out.println("* * ");
+    System.out.println("* ");
+ }   
+}
