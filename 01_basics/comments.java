10c0eb20fc6f817fb0a7ad75af5411786d1ca1ff
KRAPANSH SAHU <sahukrapansh@gmail.com>
2026-04-08 19:39:10 +0000

    add

---
+++ 01_basics/comments.java
@@ -0,0 +1,22 @@
+public class comments {
+    public static void main(String[] args) {
+        // single line comment
+
+        /*
+
+        multiline
+        comments
+
+        */
+
+        // code for divide
+
+        int a = 30;
+        int b = 2;
+
+        int div = a/b;
+        System.out.println(div);
+
+    }
+    
+}
