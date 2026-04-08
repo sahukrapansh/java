abc80dd8886cadc3852d4226d9de1574d2c8adb3
KRAPANSH SAHU <sahukrapansh@gmail.com>
2026-04-08 19:59:21 +0000

    update

--- 01_basics/userinput.java
+++ 01_basics/userinput.java
@@ -2,11 +2,14 @@ import java.util.*;
 
 public class userinput {
     public static void main(String args[]){
-        
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
 
-        System.out.println(s);
+        Scanner sc = new Scanner(System.in);
+
+        // String s = sc.next();   this print one word only
+        //  System.out.println(s);
+
+        String full= sc.nextLine();  // this print full sentence
+        System.out.println(full);
     }
     
 }
