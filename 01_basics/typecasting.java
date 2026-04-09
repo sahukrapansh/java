fa89b79aea5eeafd3a404bf4307be73f7270b2bf
KRAPANSH SAHU <sahukrapansh@gmail.com>
2026-04-09 12:45:35 +0000

    nnew file

---
+++ 01_basics/typecasting.java
@@ -0,0 +1,12 @@
+public class typecasting {
+    public static void main(String[] args) {
+
+
+        // lossy (data loss)
+         float a = 66.99f;
+         int b = (int)a;
+         System.out.println(b);
+         
+    }
+    
+}
---
+++ conversion.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class conversion {
+
+    public static void main(String[] args) {
+        
+        int a = 25;
+        float b = a;
+
+        System.out.println(b);
+
+        // conversion using scanner class
+        Scanner sc = new Scanner(System.in);
+        float c = sc.nextInt();
+
+        System.out.println(c);
+    }
+}
