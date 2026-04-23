36248c39d39e3c3a0cb4d9bf29a168e5a9891e18
KRAPANSH SAHU <sahukrapansh@gmail.com>
2026-04-23 16:10:28 +0000

    switches

---
+++ 03_conditional statement/switches.java
@@ -0,0 +1,19 @@
+public class switches {
+    public static void main(String[] args) {
+        int number = 2;
+
+        switch(number) {
+            case 1:
+                System.out.println("mango");
+                break;
+            case 2:
+                System.out.println("banana");
+                break;
+            case 3:
+                System.out.println("grapes");
+                break;
+            default:
+                System.out.println("invalid");
+        }
+    }
+}
