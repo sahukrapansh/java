429bc6cf97e529ce60d7c6995a71f8c41e621499
KRAPANSH SAHU <sahukrapansh@gmail.com>
2026-04-29 20:41:25 +0000

    whileloop

---
+++ loops/whileloop.java
@@ -0,0 +1,11 @@
+
+public class whileloop {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 100000) {
+            System.out.println("hello world");
+            counter++;
+        }
+        System.out.println("printed hw 1lakh time");
+    }
+}
